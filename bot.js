b9d7388d11557198f54b44599a897b586e6ea10e
Hvickus <abadkov2030@gmail.com>
2025-12-14 15:20:28 +0500

    Краткое описание изменений

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ bot.start(async (ctx) => {
   });
 });
 
-require("./handlers/reports/exelMenu")(bot);
+require("./handlers/reports/exсelMenu")(bot);
 require("./handlers/reports/excelReport")(bot);
 
 require("./handlers/navigation")(bot);
